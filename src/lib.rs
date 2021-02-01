8e95bb05bbee8913f92350ce7b46c9893f20a709
Joel Watson <joelwat@gmail.com>
2021-01-31 19:51:33 -0800

    Major refactor. Moves most code into lib

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,47 @@
+use std::env;
+use std::fs;
+use std::path::{ Path, PathBuf };
 
-use std::error::Error;
+use anyhow::{ bail, Result };
 
-pub fn run() -> Result<(), Box<dyn Error>> {
+pub fn run() -> Result<()> {
+    // let data_dir_contents: String;
+
+    let vars = get_env_vars()?;
+
+    let data_dir = fs::canonicalize(vars.data_dir)?;
+    let read_dir = fs::read_dir(data_dir)?;
+
+    for entry in read_dir.into_iter() {
+        let entry = entry?;
+
+        println!("File name: {:#?}", entry.file_name());
+    }
 
     return Ok(());
+}
+
+struct EnvVars {
+    data_dir: PathBuf,
+}
+
+fn get_env_vars() -> Result<EnvVars> {
+    let env_var = env::var("DATA_DIR");
+
+    if let Err(_) = env_var {
+        bail!("The DATA_DIR env var must be set");
+    }
+
+    let env_var = env_var.unwrap();
+    let data_dir = Path::new(&env_var);
+
+    if !data_dir.is_dir() {
+        bail!("DATA_DIR must be a valid directory");
+    }
+
+    let buff_path = fs::canonicalize(data_dir)?;
+
+    Ok(EnvVars {
+        data_dir: buff_path,
+    })
 }
--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,12 @@
-use std::env;
-use std::fs;
-use std::path::Path;
+use std::process;
+
+use load_covid;
 
 fn main() {
-    let data_dir: &Path;
-    // let data_dir_contents: String;
+    if let Err(err) = load_covid::run() {
+        eprintln!("This program is b0rked");
+        eprintln!("Error: {}", err);
 
-    let env_var = env::var("DATA_DIR").expect("The DATA_DIR env var must be set");
-
-    data_dir = Path::new(&env_var);
-
-    if !data_dir.is_dir() {
-        return;
-    }
-
-    let read_dir = fs::read_dir(fs::canonicalize(data_dir).unwrap()).unwrap();
-
-    for entry in read_dir.into_iter() {
-        let entry = entry.unwrap();
-
-        println!("File name: {:#?}", entry.file_name());
+        process::exit(1);
     }
 }
