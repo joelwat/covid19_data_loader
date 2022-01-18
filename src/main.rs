d2c52aed5fa2af6e2b0dd513678daea517207cd8
Joel Watson <joelwat@gmail.com>
2022-01-17 19:37:06 -0800

    Print Debug for error

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ use std::process;
 fn main() {
     if let Err(err) = load_covid::run() {
         eprintln!("This program is b0rked");
-        eprintln!("Error: {}", err);
+        eprintln!("Error: {:?}", err);
 
         process::exit(1);
     }
