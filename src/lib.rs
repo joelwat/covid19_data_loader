6adcc0131140b90e311583a30d9824dcfacf6aed
Joel Watson <joelwat@gmail.com>
2022-01-17 17:40:16 -0800

    Move EnvVars to separate module

--- src/lib.rs
+++ src/lib.rs
@@ -69,30 +69,3 @@ println!("lat_split: {:#?}", split);
 
     Ok(())
 }
-
-struct EnvVars {
-    data_dir: PathBuf,
-}
-
-fn get_env_vars() -> Result<EnvVars> {
-    dotenv().expect("Failed to read .env file.");
-
-    let env_var = env::var("DATA_DIR");
-
-    if env_var.is_err() {
-        bail!("The DATA_DIR env var must be set");
-    }
-
-    let env_var = env_var.unwrap();
-    let data_dir = Path::new(&env_var);
-
-    if !data_dir.is_dir() {
-        bail!("DATA_DIR must be a valid directory");
-    }
-
-    let buff_path = fs::canonicalize(data_dir).unwrap();
-
-    Ok(EnvVars {
-        data_dir: buff_path,
-    })
-}
